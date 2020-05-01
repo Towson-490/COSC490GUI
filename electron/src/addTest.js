d9d30c813c03e0ab4763f5f893dd268d5efdcdd6
Jose De La Rosa <josedlr93@aol.com>
2020-04-30 21:48:51 -0400

    GUI: Allow user change test values - use local storage

--- electron/src/addTest.js
+++ electron/src/addTest.js
@@ -1,95 +1,94 @@
 const { ipcRenderer } = require('electron');
+let tests = require("./scripts/tests.json")
+
+// If local storage is available, and tests is not stored, store for editing
+if (typeof(Storage) !== "undefined") {
+  if (!localStorage.getItem("tests")){
+    localStorage.setItem("tests", JSON.stringify(tests))
+  }else{
+    tests = JSON.parse(localStorage.getItem("tests"))
+    console.log(tests)
+  }
+} else {
+  // No web storage Support.
+}
+
 
 let addData = {};
-
 // Get data from main window
 ipcRenderer.on('add-data', function (e, data) {
   addData = data;
   console.log(data);
 });
 
-let tests = [
-  {
-    "routeDesc": "Number of page fonts",
-    "routeSlug": "get_fonts",
-    "desc": "Test consistency",
-    "values": {
-      "acceptable": 6,
-    },
-  },
-  {
-    "routeDesc": "Check page text",
-    "routeSlug": "get_nogo_text/text",
-    "desc": "Test terms to avoid"
-  },
-  {
-    "routeDesc": "Number of text colors",
-    "routeSlug": "get_text_colors",
-    "desc": "Test consistency",
-    "values": {
-      "acceptable": 6,
-    },
-  },
-  {
-    "routeDesc": "Number of background colors",
-    "routeSlug": "get_background_colors",
-    "desc": "Test consistency",
-    "values": {
-      "acceptable": 6,
-    },
-  },
-  {
-    "routeDesc": "Check background colors",
-    "routeSlug": "get_nogo_colors/background",
-    "desc": "Test colors to avoid"
-  },
-  {
-    "routeDesc": "Get average response times",
-    "routeSlug": "get_avg_response",
-    "desc": "Test time (ms) between domain links",
-    "values": {
-      "acceptable": 10000,
-      "backend": 5000,
-      "frontend": 5000
-    },
-  },
-  
-];
-
 function getEntries(o) {
   if (o) {
-    return `<footer class="blockquote-footer">Defaults:
+    return `<footer class="blockquote-footer">Values:
       ${Object.entries(o).map(e => { return ` [${e[0]}: ${e[1]}]` })}
     </footer>`
   }
 }
 $(window).on("load", () => {
-  for (let test of tests) {
+  n = 1
+  tests.forEach((test, index)=>{
     let addParams = test["values"]
     $("#testList").append(`
   <div class="form-check" id=testDiv>
     <input id=${test["routeSlug"]} class="form-check-input test" type="checkbox" name="test" value="${test["routeSlug"]}">
     <label class="form-check-label" for="test">${test["routeDesc"]}</label>
-    <span>${(addParams ? `<button id="edit-value-modal" type="button" class="btn btn-link btn-link" data-toggle="modal" data-target="#editModal"
-        data-whatever="@getbootstrap">${"Edit values"}</button>` : "")}</span>
+    <span>${(addParams ? `<button id="edit-test${n}-values" type="button" class="btn btn-link btn-link" data-toggle="modal" data-target="#editModal"
+        data-whatever="@getbootstrap">Edit Values</button>` : "")}</span>
   </div>
   <blockquote class="blockquote">
     <footer class="blockquote-footer">${test["desc"]}</footer>
     ${(addParams ? getEntries(test["values"]) : "")}
   </blockquote>
   `);
-    if (addParams) {    
-      let $routeSlug = $(`#${test["routeSlug"]}`) 
-      $routeSlug.val($routeSlug.val()+"?values=true")
-      Object.entries(addParams).forEach(([key, value]) => { 
-        console.log(`&${key}=${value}`)
+
+    $(`#edit-test${n++}-values`).on("click", () => {
+      $("#editModalLabel").text(`Edit Values: ${test.routeDesc}`)
+      $("#modal-form").empty()
+      for (v in test.values) {
+        console.log(v)
+        $("#modal-form").append(
+          `
+          <p>${v}</p>
+          <input class="form-control"
+            id=${v}
+            name=${index} 
+            type="${typeof (test.values[v])}"
+            ${typeof (test.values[v]) == "number" ? "min=0" : ""} 
+            value=${test.values[v]}>
+          <hr>
+          `
+        )
+      }
+    });
+
+
+    if (addParams) {
+      let $routeSlug = $(`#${test["routeSlug"]}`)
+      $routeSlug.val($routeSlug.val() + "?values=true")
+      Object.entries(addParams).forEach(([key, value]) => {
+        console.log(`test: ${test["routeDesc"]} &${key}=${value}`)
         $routeSlug.val((i, val) => {
           return val + `&${key}=${value}`
-        }) 
+        })
       })
     }
-  }
+  })
 });
+
+function updateValues(e) {
+  $("#modal-form :input").each(function(){
+    tests[$(this).prop("name")]["values"][$(this).prop("id")] = parseInt($(this).val())
+    localStorage.setItem("tests", JSON.stringify(tests))
+    location.reload();
+    //return false;
+  })
+  
+  
+}
 
 // Handle submit button
 function getCheckedBoxes(e) {
@@ -102,7 +101,7 @@ function getCheckedBoxes(e) {
 
     if (checkedBoxes.length != 0) {
       let boxes = {}
-      for (box of checkedBoxes){
+      for (box of checkedBoxes) {
         boxes[box.nextElementSibling.innerText] = box.value;
       }
       data.boxes = boxes;
